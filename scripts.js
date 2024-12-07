e31992450ec8fc6a79af0e021643239b6dec7306
rajeshkumarpathak01 <rajprofessional42@gmail.com>
2024-12-07 16:58:50 +0530

    lattest change

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
-
     setInterval(() => {
         const times = new Date().toLocaleTimeString();
         const val = document.querySelector('#timeSpan');
-        val.style.color = 'rgb(2, 0, 36)'
         val.textContent = times;
 
     }, 1000);
@@ -13,55 +10,75 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const btnGuess = document.querySelector('#submitguess');
     let prevGuessValue = [];
-    let playGame = false;
+    let playGame = true;
     let remainingGuess = 10;
     let isStartedGame = false;
+    const prevGuess = document.querySelector('#guesses');
+    const confettiContainer = document.querySelector('.confetti-container');
+
+    const randNum = Math.floor(Math.random() * 20 + 1);  
 
     btnGuess.addEventListener('click', () => {
-        let inptGuess = parseInt(document.querySelector('#guess').value);
-        const randNum = Math.floor(Math.random() * 100 + 1);
-        const prevGuess = document.querySelector('#guesses');
+        let inptGuess = parseInt(document.querySelector('#guess').value);    
         const remGuess = document.querySelector('#lastResult');
         const message = document.querySelector('#lowOrHi');
         const guessContainer = document.querySelector('#guessRemaingContainer');
         const randNumber = document.querySelector('#randNum');
 
         if (playGame) {
+            document.querySelector('#guess').style.visibility = 'visible';
             document.querySelector('#guess').style.backgroundColor = 'white';
             document.querySelector('#guess').disabled = false;
+            document.querySelector('#guess').textContent='';
+            document.querySelector('#guess').value = '';
             btnGuess.textContent = 'Submit Guess'
+            randNumber.textContent=''
             message.textContent = '';
             prevGuess.innerText = ''
             remGuess.textContent = '10'
             remainingGuess = 10;
             isStartedGame = true
             playGame = false;
+            confettiContainer.style.display = 'none';
 
         }
 
         if (inptGuess && !isNaN(inptGuess) && inptGuess > 0 && inptGuess < 101) {
             remGuess.textContent = --remainingGuess;
 
-            if (inptGuess < randNum) {
-                message.textContent = 'Your guess number is too low'
-                message.style.color = 'red'
+             if (inptGuess < randNum) {
+                 message.textContent = 'Your guess number is too low'
+                 message.style.color = 'red'
+                 //randNumber.textContent = `Better luck next time -😁 ${randNum} 😁`;
+                 calculatePreviousValue(inptGuess);
 
 
-            } else if (inptGuess > randNum) {
-                message.textContent = 'Your guess number is too high';
-                message.style.color = 'red'
-            } else {
-                message.textContent = '🎉 Congratulation Your guess is correct 🎉'
+             } else if (inptGuess > randNum) {
+                 message.textContent = 'Your guess number is too high';
+                 message.style.color = 'red'
+                // randNumber.textContent = `Better luck next time -😁 ${randNum} 😁`;
+                 calculatePreviousValue(inptGuess);
+             } 
+            
+            
+
+
+            if (inptGuess == randNum) {
                 guessContainer.style.display = 'hidden';
-                document.querySelector('#guess').style.backgroundColor = 'red';
-                document.querySelector('#guess').disabled = true;
-                startButton();
+                document.querySelector('#guess').style.visibility = 'hidden';
+                randNumber.textContent = '🎉Congratulation! Your guess is correct  and  You have won myntra coupon code- MYNTRA300🎉';  
+                btnGuess.textContent = 'Start New Game',
+                confettiContainer.style.display = 'block';
+                document.querySelector('.resultParas').style.display='none'     
+                btnGuess.textContent = 'Start New Game'
+                prevGuessValue = [];
+                
+                playGame = true;
+                
 
             }
 
-            prevGuessValue.push(inptGuess);
-            prevGuess.innerText = `${prevGuessValue}, `
-            randNumber.textContent = `Better luck next time -😁 ${randNum} 😁`;
+            
         } else {
             if (isStartedGame) {
                 message.textContent = '';
@@ -74,14 +91,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         }
         
-        document.querySelector('#guess').autofocus;
         
-        document.querySelector('#guess').value = '';
 
         if (remainingGuess == 0) {
             document.querySelector('#guess').style.backgroundColor = 'red';
             document.querySelector('#guess').disabled = true;
-            message.textContent = 'Your chances is over';
+            message.textContent = 'Game Over! You ran out of guesses.';
+            randNumber.textContent = `Better luck next time -😁 ${randNum} 😁`;
+           
             startButton();
 
         }
@@ -92,9 +109,16 @@ document.addEventListener('DOMContentLoaded', () => {
         remainingGuess = 0;
         playGame = true;
 
-
+    }
+    function calculatePreviousValue(inptGuess) {
+        prevGuessValue.push(inptGuess);
+        prevGuess.innerText = ` ${prevGuessValue},`
+        document.querySelector('#guess').autofocus;
+        document.querySelector('#guess').value = '';
     }
 
 
+
+    
 
 })
